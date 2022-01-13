6558816865df80a5c3de3b539eeeac610b08781f
MertPlc <mertpalacioglu@gmail.com>
2022-01-13 12:58:54 +0300

    navbar

--- quick-note-ui/src/App.js
+++ quick-note-ui/src/App.js
@@ -1,15 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react';
 import './App.css';
 import Enterance from './Enterance';
 import NoteBook from './NoteBook';
 import AppContext from './AppContext';
+import { Button } from 'bootstrap';
 
 function App() {
   const [noteBook, setNoteBook] = useState(null);
 
   return (
-    <AppContext.Provider value={noteBook, setNoteBook}>
-      <div className="App">
+    <AppContext.Provider value={{noteBook, setNoteBook}}>
+      <div className="App w-100 h-100">
         {noteBook ? <NoteBook /> : <Enterance />}
       </div>
     </AppContext.Provider>
--- quick-note-ui/src/NoteBook.js
+++ quick-note-ui/src/NoteBook.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import AppContext from './AppContext';
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -12,17 +13,36 @@ function NoteBook() {
             .then(function(response) {
                 setNotes(response.data);
             });
-    }, [])
+    }, []);
+    const handleClose = function(event) {
+        event.preventDefault();
+        ctx.setNoteBook(null);
+    };
 
     return (
         <div>
-            <h1>Notes of '{ctx.noteBook.name}' will be here..</h1>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand>QuickNote</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link onClick={handleClose}>Close the Notebook</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Row>
+                <Col sm={4} md={3}>col1</Col>
+                <Col sm={8} md={9}>col2</Col>
+            </Row>
+            <h1>Notes of '{ctx.noteBook.name}'</h1>
 
             <ul>
                 { notes.map(x => <li key={x.id}>{ x.title}</li>) }
             </ul>
         </div>
-    )
+    );
 }
 
-export default NoteBook
+export default NoteBook;
